14de9eaed6789439fba4357fa11c33c42cbabbd2
johnnysee <js.johnny.see@gmail.com>
2021-10-13 22:52:34 +0200

    Installed react-router-dom for links, styles.css to put footer in bottom, semantic-ui, homepage as default route, added header footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import HomePage from "./HomePage";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const HomePage = () => {
+  return (
+    <>
+      <Container>
+        <p>Content</p>
+      </Container>
+    </>
+  );
+};
+
+export default HomePage;
