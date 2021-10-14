79b618034cfebc64ba93987e236c7bcec06242f2
maxarvid <max.arvid.anderson@gmail.com>
2021-10-14 23:03:22 +0200

    Refactors axios call function into separate module, uses async function inside useEffect to call it

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,31 +1,30 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Grid } from "semantic-ui-react";
-// import { articlesIndex } from "../modules/apiHelper";
+import { articlesIndex } from "../modules/apiHelper";
 
 import ArticleItem from "./ArticleItem";
 
 const HomePage = () => {
   const [articles, setArticles] = useState([]);
 
-  // const articlesIndex = async () => {
-  //   const response = await axios.get("/api/articles/");
-  //   setarticles(response.data.articles);
-  // };
-
   useEffect(() => {
-    axios.get("/api/articles/").then((response) => {
-      setArticles(response.data.articles);
-    });
+    (async () => {
+      const articlesResponse = await articlesIndex()
+      setArticles(articlesResponse)
+    })()
   }, []);
 
   let articleList = articles.map((articleItem, index) => {
-    return <ArticleItem articleItem={articleItem} index={index} key={articleItem.id} />;
+    return (
+      <ArticleItem
+        articleItem={articleItem}
+        index={index}
+        key={articleItem.id}
+      />
+    );
   });
 
   return (
-    // <div>{articleList}</div>
-
     <Grid>
       <Grid.Row>
         <Grid.Column column="equal" data-cy="news-section">
--- src/modules/apiHelper.js
+++ src/modules/apiHelper.js
@@ -7,16 +7,6 @@ const articlesIndex = async () => {
   } catch (error) {
     errorHandler(error);
   }
-
-  // async show(id) {
-  //   try {
-
-  //     const response = await axios.get(`/api/articles/${id}`);
-  //     return response.data.article;
-  //   } catch (error) {
-  //     errorHandler(error)
-  //   }
-  // },
 };
 
 const errorHandler = (error) => {
