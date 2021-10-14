06894bec8014c5d62038bf6feb4c048d739d7186
johnnysee <js.johnny.see@gmail.com>
2021-10-14 09:58:51 +0200

    Created components and styles folders to organize better, also renamed App.css to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import HomePage from "./HomePage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./components/HomePage";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
