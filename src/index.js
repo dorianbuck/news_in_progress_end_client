b56b708c1a75ab8016d1f0b73c9282cef962ed60
August <august.enhager@gmail.com>
2021-10-15 14:23:44 +0200

    redux basic setup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "semantic-ui-css/semantic.min.css";
 import "./styles/index.css";
+import { Provider } from "react-redux"
+import configureStore from "./state/store/configureStore";
+
+const store = configureStore()
+
+window.store = store
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
---
+++ src/state/store/initialState.js
@@ -0,0 +1,5 @@
+const initialState = {
+  message: "Hello wolrd from redux",
+};
+
+export default initialState;
