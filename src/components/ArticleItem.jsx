e74a1cd799d7748e2c7f01e27613198c1277d5a3
maxarvid <max.arvid.anderson@gmail.com>
2021-10-18 11:19:17 +0200

    Removes unused application state dispatch from ArticleItems component

--- src/components/ArticleItem.jsx
+++ src/components/ArticleItem.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import { Segment, Grid } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
 
 const ArticleItem = ({ articleItem, id }) => {
-  const dispatch = useDispatch();
-
-
   return (
     <Grid.Column width="4" data-cy="news-section">
       <Segment
         data-cy={`article-${id}`}
         as={Link}
         to={{ pathname: `/article/${id}` }}
-        onClick={() =>
-          dispatch({
-            type: "SET_ARTICLE_ID",
-            payload: id,
-          })
-        }
         basic
       >
         <h3 data-cy="title"> {articleItem.title}</h3>
