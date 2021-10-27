eff9da3d7b87a69d92ef1aed6ed89109fc043b70
Fraseer <Fraser.Hughes@btinternet.com>
2021-10-27 21:17:29 +0200

    adds toast for sign in

--- cypress/integration/userCanSignIn.feature.js
+++ cypress/integration/userCanSignIn.feature.js
@@ -24,4 +24,17 @@ describe("User can sign into their account", () => {
   it("is expected to have two input fields and a submit button", () => {
     cy.get("[data-cy=sign-in-form]").children().should("have.length", 3);
   });
+
+  describe("when the sign in is successful", () => {
+    beforeEach(() => {
+      cy.get("[data-cy=email-input]").type("user@email.com");
+      cy.get("[data-cy=password-input]").type("password");
+      cy.get("[data-cy=btn-sign-in]").click();
+    });
+    it("is expected to display a success message", () => {
+      cy.get("[data-cy=sign-in-toast]").within(() => {
+        cy.contains("Sign In Successful");
+      });
+    });
+  });
 });
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Container, Form, Button, Input } from "semantic-ui-react";
 import { useTranslation } from "react-i18next";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SignIn = () => {
   const { t } = useTranslation();
+  const notify = () => toast.success("Sign In Successful");
 
   return (
     <Container>
-      <Form data-cy="sign-in-form" >
+      <Form data-cy="sign-in-form">
         <Form.Field
           name="email"
           data-cy="email-input"
@@ -23,11 +26,25 @@ const SignIn = () => {
           type="password"
         />
         <Form.Field
-          data-cy="btn-signin"
+          data-cy="btn-sign-in"
           control={Button}
           content={t("submit")}
+          onClick={notify}
         />
       </Form>
+      <div data-cy="sign-in-toast">
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </div>
     </Container>
   );
 };
