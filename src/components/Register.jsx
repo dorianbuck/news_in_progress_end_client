5471af203af4e4894ebd6d40f7815720978ff376
Dorian Buck <dorianbuck@gmail.com>
2021-10-27 16:29:46 +0200

    adds const for name in register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -21,6 +21,7 @@ const Register = () => {
   const handleAuthentication = async (event) => {
     event.preventDefault();
     const form = event.target;
+    const name = form.name.value
     const email = form.email.value;
     const password = form.password.value;
     const confirmPassword = form.confirmPassword.value;
