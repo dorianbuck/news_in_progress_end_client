2b512760354033b78e6a5b17a0a366126f9bf30e
Fraseer <Fraser.Hughes@btinternet.com>
2021-10-29 09:27:49 +0200

    switches from Redirect to useHistory on sign in component

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Form, Button, Input } from "semantic-ui-react";
 import { useTranslation } from "react-i18next";
 import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import auth from "../modules/auth";
 import { useDispatch } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import auth from "../modules/auth";
+import delay from "delay";
+import "react-toastify/dist/ReactToastify.css";
 
 const SignIn = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const { t } = useTranslation();
-  const [redirect, setRedirect] = useState(false);
-  const delay = require("delay");
 
   const handleSignIn = async (event) => {
     event.preventDefault();
@@ -25,58 +25,53 @@ const SignIn = () => {
         type: "SET_CURRENT_USER",
         payload: signInStatus.data,
       });
+
       toast.success(t("signInMessage"));
 
       await delay(2000);
-      setRedirect(true);
+      history.push("/");
     } catch (error) {
       toast.error(error.response.data.errors[0]);
     }
   };
 
   return (
-    <>
-      {redirect ? (
-        <Redirect to="/" />
-      ) : (
-        <Container>
-          <Form data-cy="sign-in-form" onSubmit={handleSignIn}>
-            <Form.Field
-              name="email"
-              data-cy="email-input"
-              control={Input}
-              label="Email"
-              placeholder="example@email.com"
-            />
-            <Form.Field
-              name="password"
-              data-cy="password-input"
-              control={Input}
-              label={t("password")}
-              type="password"
-            />
-            <Form.Field
-              data-cy="btn-sign-in"
-              control={Button}
-              content={t("submit")}
-            />
-          </Form>
-          <div data-cy="sign-in-toast">
-            <ToastContainer
-              position="top-center"
-              autoClose={2000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-            />
-          </div>
-        </Container>
-      )}
-    </>
+    <Container>
+      <Form data-cy="sign-in-form" onSubmit={handleSignIn}>
+        <Form.Field
+          name="email"
+          data-cy="email-input"
+          control={Input}
+          label="Email"
+          placeholder="example@email.com"
+        />
+        <Form.Field
+          name="password"
+          data-cy="password-input"
+          control={Input}
+          label={t("password")}
+          type="password"
+        />
+        <Form.Field
+          data-cy="btn-sign-in"
+          control={Button}
+          content={t("submit")}
+        />
+      </Form>
+      <div data-cy="sign-in-toast">
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </div>
+    </Container>
   );
 };
 
