4a1347bc9a64ae1fdb8eab045bc6723badae0f6a
Dorian Buck <dorianbuck@gmail.com>
2021-10-31 16:11:18 +0100

    adds fix for deployment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import "./styles/index.css";
 import axios from "axios";
 import "./i18n";
 import { StripeProvider } from "react-stripe-elements";
-import PaymentModal from "./components/PaymentModal";
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   axios.defaults.baseURL = "http://localhost:3000";
@@ -19,9 +18,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 window.store = store;
 ReactDOM.render(
   <Provider store={store}>
-    <StripeProvider apiKey="pk_test_12345">
+    <StripeProvider apiKey="pk_test_QicERB8w3kyqaYW3hUUQylRH">
       <App />
-      
     </StripeProvider>
   </Provider>,
   document.getElementById("root")
